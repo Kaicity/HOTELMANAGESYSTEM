193190d4ca600003ee1bfbbe41fdd49e36f05fa2
Nguyễn Minh Thông <80609391+Kaicity@users.noreply.github.com>
2023-06-24 23:23:59 +0700

    Push project first level 1

--- THUEPHONG/Program.cs
+++ THUEPHONG/Program.cs
@@ -24,6 +24,7 @@ namespace THUEPHONG
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMainFull());
+            //hello
 
             /*if (File.Exists("connectdb.dba"))
             {
